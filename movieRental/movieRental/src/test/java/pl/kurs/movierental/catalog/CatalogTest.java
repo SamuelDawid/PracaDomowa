ec8269ee566028dcec9d2b52f1bad8a87339b81b
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-05 18:24:57 +0200

    Catalog Test

--- movieRental/movieRental/src/test/java/pl/kurs/movierental/catalog/CatalogTest.java
+++ movieRental/movieRental/src/test/java/pl/kurs/movierental/catalog/CatalogTest.java
@@ -1,22 +1,42 @@
 package pl.kurs.movierental.catalog;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.kurs.movierental.domain.Movie;
 import pl.kurs.movierental.domain.enums.Category;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CatalogTest {
+    Catalog<Movie> catalog;
 
-    @Test
-    void createCatalog() {
-        Catalog<Movie> movies = new Catalog<>();
-        movies.add(1, new Movie(1, "Shrek", "Adamson", 2001, Category.FAMILY));
-
-        assertThat(movies.find(1)).isPresent();
-        assertThat(movies.find(99)).isEmpty();
-        assertThat(movies.size()).isEqualTo(1);
+    @BeforeEach
+    void setUp() {
+        catalog = new Catalog<>();
+        catalog.add(1, new Movie(1, "Shrek", "Adamson", 2001, Category.FAMILY));
     }
 
+    @Test
+    void createCatalogCheckFindAndSize() {
+        assertThat(catalog.find(1)).isPresent();
+        assertThat(catalog.find(99)).isEmpty();
+        assertThat(catalog.size()).isEqualTo(1);
+    }
+    @Test
+    void CatalogContains(){
+        assertThat(catalog.contains(1)).isTrue();
+        assertThat(catalog.contains(99)).isFalse();
+    }
+    @Test
+    void addThrIllegalArgumentException(){
+        assertThrows(IllegalArgumentException.class,
+                () -> catalog.add(1, new Movie(1, "Matrix", "Wachowski", 1999, Category.ACTION)));
+    }
+    @Test
+    void addNull(){
+        assertThrows(NullPointerException.class,
+                () -> catalog.add(2, null));
+    }
 
 }
