562c99e4609ab8402758b8d1d26d1b63ecc4aafc
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-12 18:06:23 +0200

    zadanie 3 i 4

--- Collections/src/main/java/org/yellowflash/kartaPracy/zad1/Kalkulator.java
+++ Collections/src/main/java/org/yellowflash/kartaPracy/zad1/Kalkulator.java
@@ -1,40 +1,15 @@
 package org.yellowflash.kartaPracy.zad1;
 
-import java.util.Scanner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Kalkulator {
-    static Double calculate(double a, double b, BiFunction<Double, Double, Double> op) {
+    public static Integer calculate(int a, int b, BiFunction<Integer, Integer, Integer> op) {
         return op.apply(a, b);
     }
+}
 
-    public static void main(String[] args) {
-        BiFunction<Double, Double, Double> add = (a, b) -> a + b;
-        BiFunction<Double, Double, Double> sub = (a, b) -> a - b;
-        BiFunction<Double, Double, Double> mul = (a, b) -> a * b;
-        BiFunction<Double, Double, Double> div = (a, b) -> a / b;
-        BiFunction<Double, Double, Double> mod = (a, b) -> a % b;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Podaj a: ");
-        double a = sc.nextDouble();
-        System.out.print("Podaj b: ");
-        double b = sc.nextDouble();
-        System.out.print("Podaj operator (+ - * /): ");
-        String op = sc.next();
-        if((op.equals("/") || op.equals("%")) && (b == 0)){
-            System.out.println("You can't divide by 0");
-        }else {
-            BiFunction<Double, Double, Double> chosen = switch (op) {
-                case "+" -> add;
-                case "-" -> sub;
-                case "*" -> mul;
-                case "/" -> div;
-                case "%" -> mod;
-                default -> throw new IllegalArgumentException("Nieznany operator: " + op);
-            };
-            System.out.println("Wynik: " + calculate(a, b, chosen));
-        }
+
         /*
         Pytania kontrolne
         Jakie typy są w BiFunction<Integer, Integer, Integer> (kolejność: argument 1, argument 2, wynik)?
@@ -51,5 +26,4 @@ the function result
 
 
         
-    }
-}
+
--- Collections/src/main/java/org/yellowflash/zadanie/App.java
+++ Collections/src/main/java/org/yellowflash/zadanie/App.java
@@ -1,7 +1,11 @@
 package org.yellowflash.zadanie;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.function.Predicate;
+
+import static org.yellowflash.kartaPracy.zad1.Kalkulator.calculate;
 
 public class App {
     public static void main(String[] args) {
@@ -14,6 +18,8 @@ public class App {
         new Thread(runnable1).start();
         //endregion
         //region  zadanie2
+
+        //Function<String,String> deleteSpace = String::trim;
         Function<String,String> toLower = String::toLowerCase;
         Function<String,String> replaceSpace = s ->s.replaceAll(" ","");
         String[] loginy = {"  Adam  ", "ANIA K  ", "  k o w a l ","   lol   "};
@@ -25,7 +31,46 @@ public class App {
         //endregion
 
         //region zadanie3
+        String samogloski ="aeiouyAEIOUY";
+        Function<String, Integer> textLength = string -> string.replaceAll(" ","").length();
+        Function<String, Integer> countVowels  =  string -> {
+          int count = 0;
+            for (int i = 0; i < string.length(); i++) {
+                char c = string.charAt(i);
+                if(samogloski.indexOf(c) >= 0) count++;
+            }
+            return count;
+        };
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("Liczba znakow bez spacji: " + textLength.apply(text));
+        System.out.println("Liczba samoglosek: " + countVowels.apply(text));
+        //endregion
+        //region zadanie4
 
+        BiFunction<Integer,Integer,Integer> add = Integer::sum;
+        BiFunction<Integer,Integer,Integer> sub = (a,b) -> a - b;
+        BiFunction<Integer,Integer,Integer> mul = (a,b) -> a * b;
+        BiFunction<Integer,Integer,Integer> div = (a,b) -> a / b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Podaj a: ");
+        int a = sc.nextInt();
+        System.out.print("Podaj b: ");
+        int b = sc.nextInt();
+        System.out.print("Podaj operator (+ - * /): ");
+        String op = sc.next();
+        if((op.equals("/") || op.equals("%")) && (b == 0)){
+            System.out.println("You can't divide by 0");
+        }else {
+            BiFunction<Integer, Integer, Integer> chosen = switch (op) {
+                case "+" -> add;
+                case "-" -> sub;
+                case "*" -> mul;
+                case "/" -> div;
+                default -> throw new IllegalArgumentException("Nieznany operator: " + op);
+            };
+            System.out.println("Wynik: " + calculate(a ,b ,chosen));
         //endregion
     }
+}
 }
