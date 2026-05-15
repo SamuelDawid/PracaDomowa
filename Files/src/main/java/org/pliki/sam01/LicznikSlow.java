3ad9118119a1f07d90741d5514a7850bb50f2e9b
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-15 12:17:40 +0200

    sam01

--- Files/src/main/java/org/pliki/sam01/LicznikSlow.java
+++ Files/src/main/java/org/pliki/sam01/LicznikSlow.java
@@ -1,7 +1,29 @@
 package org.pliki.sam01;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class LicznikSlow {
     public static void main(String[] args) {
+
+        Path path = args.length < 1 ? Paths.get("dane","sam01","tekst.txt") : Path.of(args[0]);
+        System.out.println("Szukam pliku: " + path.toAbsolutePath());
+        if(!Files.exists(path)){
+            System.out.println("file does not exits: " );
+        }
+        try {
+            List<String> odczyt = Files.readAllLines(path);
+            System.out.println("Liczba lini: " + odczyt.size());
+
+        } catch (IOException e) {
+            System.out.println("Odczyt sie nie udal.");
+        }
+
 
     }
 }
