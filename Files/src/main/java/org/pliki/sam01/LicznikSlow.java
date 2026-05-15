4e8e94baca338a70d6f7cd172b41398dd904a4c8
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-15 12:46:24 +0200

    sam01 - done and tested

--- Files/src/main/java/org/pliki/sam01/LicznikSlow.java
+++ Files/src/main/java/org/pliki/sam01/LicznikSlow.java
@@ -12,18 +12,45 @@ public class LicznikSlow {
     public static void main(String[] args) {
 
         Path path = args.length < 1 ? Paths.get("dane","sam01","tekst.txt") : Path.of(args[0]);
-        System.out.println("Szukam pliku: " + path.toAbsolutePath());
+        Path path1 = args.length < 1 ? Paths.get("dane","sam01","pusty.txt") : Path.of(args[0]);
+        Path path2 = args.length < 1 ? Paths.get("dane","sam01","remis.txt") : Path.of(args[0]);
+
+        printPartOne(path);
+        printPartOne(path1);
+        printPartOne(path2);
+
+
+    }
+
+    static void printPartOne(Path path){
         if(!Files.exists(path)){
-            System.out.println("file does not exits: " );
+            System.out.println("file does not exits: " +path.toAbsolutePath() );
         }
         try {
             List<String> odczyt = Files.readAllLines(path);
             System.out.println("Liczba lini: " + odczyt.size());
+            int words =0;
+            long charactersCount = 0;
+            String longestWord = "";
+            for (String line : odczyt) {
+                charactersCount+= line.length() - line.replace(" ", "").length();
+                String[] parts = line.split(" ");
+                words += parts.length;
+                for (String s : parts){
+                    longestWord = longestWord.length() < s.length() ? s : longestWord;
+                    charactersCount += s.length();
+                }
 
+            }
+            System.out.println("liczbę słów: " +words);
+            System.out.println("liczbę znaków: " +charactersCount);
+            if (longestWord.isEmpty()) {
+                System.out.println("Najdluzsze slowo: brak");
+            } else {
+                System.out.println("Najdluzsze slowo: " + longestWord);
+            }
         } catch (IOException e) {
             System.out.println("Odczyt sie nie udal.");
         }
-
-
     }
 }
