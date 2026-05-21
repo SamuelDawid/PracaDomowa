e5fe50b9ec01d593a403da1f5fde55d09e240556
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-21 13:17:57 +0200

    zad02

--- Multithreading/src/main/java/org/yellowflash/zad02/zad02.java
+++ Multithreading/src/main/java/org/yellowflash/zad02/zad02.java
@@ -1,14 +1,55 @@
 package org.yellowflash.zad02;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class zad02 {
-    public static void main(String[] args) {
+public class zad02 extends Thread{
+
+    public static void main(String[] args) throws InterruptedException {
         int[] numbers = new int[1_000_000];
+        long singleThreadSum = 0;
         Random rng = new Random(55);
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = rng.nextInt(0,10000);
+            numbers[i] = rng.nextInt(10000);
+            singleThreadSum += numbers[i];
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("how many parts: ");
+        int k = sc.nextInt();
+        long[] partialSums = new long[k];
+        Thread[] threads = new Thread[k];
+        int fragmentSize = numbers.length / k;
+        for (int i = 0; i < k; i++) {
+            int end;
+            int threadIndex = i;
+            int start = i * fragmentSize;
+            if(i == k -1){
+                end = numbers.length;
+            }else
+                end = start + fragmentSize;
+
+            threads[i] = new Thread(() ->{
+                long localsum = 0;
+                for (int j = start; j < end; j++) {
+                    localsum += numbers[j];
+                }
+                partialSums[threadIndex] =localsum;
+            });
+
+            threads[i].start();
         }
 
+        for(Thread t : threads) t.join();
+
+        long parallelSum = 0;
+
+        for (long partial : partialSums) parallelSum += partial;
+
+        System.out.println("Single thread sum: " + singleThreadSum);
+        System.out.println("Parallel sum:      " + parallelSum);
+
+        System.out.println(singleThreadSum == parallelSum);
     }
 }
