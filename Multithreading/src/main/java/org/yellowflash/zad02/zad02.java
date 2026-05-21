2e0b35ffb949fc008913015eb999ea1e52276d05
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-21 14:37:44 +0200

    zadanie03

--- Multithreading/src/main/java/org/yellowflash/zad02/zad02.java
+++ Multithreading/src/main/java/org/yellowflash/zad02/zad02.java
@@ -9,18 +9,25 @@ public class zad02 extends Thread{
 
     public static void main(String[] args) throws InterruptedException {
         int[] numbers = new int[1_000_000];
+
         long singleThreadSum = 0;
         Random rng = new Random(55);
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rng.nextInt(10000);
+        }
+        long startSingle = System.nanoTime();
+        for (int i = 0; i < numbers.length; i++) {
             singleThreadSum += numbers[i];
         }
+        long endSingle = System.nanoTime();
+
         Scanner sc = new Scanner(System.in);
         System.out.println("how many parts: ");
         int k = sc.nextInt();
         long[] partialSums = new long[k];
         Thread[] threads = new Thread[k];
         int fragmentSize = numbers.length / k;
+        long startParallel = System.nanoTime();
         for (int i = 0; i < k; i++) {
             int end;
             int threadIndex = i;
@@ -42,14 +49,19 @@ public class zad02 extends Thread{
         }
 
         for(Thread t : threads) t.join();
-
+        long endParallel = System.nanoTime();
         long parallelSum = 0;
 
         for (long partial : partialSums) parallelSum += partial;
 
         System.out.println("Single thread sum: " + singleThreadSum);
         System.out.println("Parallel sum:      " + parallelSum);
+        System.out.println("Single thread time: "
+                + (endSingle - startSingle) / 1_000_000.0 + " ms");
 
+        System.out.println("Parallel time: "
+                + (endParallel - startParallel) / 1_000_000.0 + " ms");
         System.out.println(singleThreadSum == parallelSum);
     }
+
 }
