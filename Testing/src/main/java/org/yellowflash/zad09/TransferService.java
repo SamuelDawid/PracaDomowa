d9d786132edb9257ac4aa46bd3e370f535bba771
CodingChef <samuelkrzyszpien@gmail.com>
2026-05-29 11:22:49 +0200

    zad09-All

--- Testing/src/main/java/org/yellowflash/zad09/TransferService.java
+++ Testing/src/main/java/org/yellowflash/zad09/TransferService.java
@@ -50,4 +50,46 @@ public class TransferService {
                 fromAccountNumber, amount, toAccountNumber
         );
     }
+    public void transferWithLimit(String fromAccountNumber, String toAccountNumber, double amount, double dailyLimit){
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Kwota przelewu musi być większa od zera");
+        }
+        if(amount > dailyLimit) {
+            auditLogger.logTransfer(new TransferLog(
+                    fromAccountNumber, toAccountNumber, amount, false, "Dzienny Limit przekroczony"
+            ));
+            throw new DailyLimitExceededException();
+        }
+
+        Account fromAccount = accountRepository.findByAccountNumber(fromAccountNumber)
+                .orElseThrow(() -> new AccountNotFoundException(fromAccountNumber));
+
+        Account toAccount = accountRepository.findByAccountNumber(toAccountNumber)
+                .orElseThrow(() -> new AccountNotFoundException(toAccountNumber));
+
+        if (fromAccount.getBalance() < amount) {
+            auditLogger.logTransfer(new TransferLog(
+                    fromAccountNumber, toAccountNumber, amount, false, "Brak środków"
+            ));
+            notificationService.sendTransferFailure(
+                    fromAccountNumber, amount, "Brak wystarczających środków"
+            );
+            throw new InsufficientFundsException(
+                    fromAccountNumber, amount, fromAccount.getBalance()
+            );
+        }
+
+        fromAccount.setBalance(fromAccount.getBalance() - amount);
+        toAccount.setBalance(toAccount.getBalance() + amount);
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+
+        auditLogger.logTransfer(new TransferLog(
+                fromAccountNumber, toAccountNumber, amount, true, null
+        ));
+        notificationService.sendTransferConfirmation(
+                fromAccountNumber, amount, toAccountNumber
+        );
+    }
 }
--- Testing/src/test/java/org/yellowflash/zad09/TransferServiceTest.java
+++ Testing/src/test/java/org/yellowflash/zad09/TransferServiceTest.java
@@ -1,6 +1,6 @@
 package org.yellowflash.zad09;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.within;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -24,32 +23,145 @@ class TransferServiceTest {
 
     @InjectMocks
     TransferService service;
+    Account senderAccount;
+    Account receiverAccount;
 
+    @BeforeEach
+    void setUp(){
+        senderAccount = new Account("ACC-1", "Jan Kowalski", 100.0);
+        receiverAccount = new Account("ACC-2", "Anna Nowak", 500.0);
+    }
 
     @Nested
     class SuccessfulTransferTests{
-        Account accountOne = new Account("123","Sam",1000);
-        Account accountTwo = new Account("321","NieSam",500);
-
+        @BeforeEach
+        void setUp(){
+            senderAccount = new Account("ACC-1", "Jan Kowalski", 1000.0);
+            receiverAccount = new Account("ACC-2", "Anna Nowak", 500.0);
+        }
         @Test
         void shouldTransferMoneyBetweenAccounts(){
            //arrange
-            when(accountRepository.findByAccountNumber("123")).thenReturn(Optional.of(accountOne));
-            when(accountRepository.findByAccountNumber("321")).thenReturn(Optional.of(accountTwo));
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.of(receiverAccount));
             //act
-            service.transfer("123","321",200.0);
-
-
+            service.transfer("ACC-1","ACC-2",200.0);
             //assert
-            assertThat(accountOne.getBalance()).isEqualTo(800);
-            assertThat(accountTwo.getBalance()).isEqualTo(700);
+            assertThat(senderAccount.getBalance()).isEqualTo(800);
+            assertThat(receiverAccount.getBalance()).isEqualTo(700);
 
-            verify(notificationService).sendTransferConfirmation("123",200.0,"321");
-            verify(accountRepository).save(accountOne);
-            verify(accountRepository).save(accountTwo);
-            verify(notificationService,never()).sendTransferFailure("123",200.0,"321");
+            verify(notificationService).sendTransferConfirmation("ACC-1",200.0,"ACC-2");
+            verify(accountRepository).save(senderAccount);
+            verify(accountRepository).save(receiverAccount);
+            verify(notificationService,never()).sendTransferFailure("ACC-1",200.0,"ACC-2");
             verify(auditLogger).logTransfer(any());
         }
 
     }
+    @Nested class InsufficientFundsTests{
+        @BeforeEach
+        void setUpNestedFundTest(){
+            senderAccount = new Account("ACC-1", "Jan Kowalski", 100.0);
+            receiverAccount = new Account("ACC-2", "Anna Nowak", 500.0);
+        }
+        @Test
+        void shouldThrowExceptionWhenInsufficientFunds(){
+                //arrange
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.of(receiverAccount));
+            // act + assert
+            assertThrows(InsufficientFundsException.class, () ->  service.transfer("ACC-1","ACC-2",500.0));
+            verify(accountRepository,never()).save(senderAccount);
+            verify(auditLogger).logTransfer(any());
+            verify(notificationService).sendTransferFailure(eq("ACC-1"),eq(500.0),anyString());
+        }
+        @Test
+        void shouldNotSendSuccessNotificationWhenInsufficientFunds(){
+            //arrange
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.of(receiverAccount));
+            // act + assert
+            assertThrows(InsufficientFundsException.class, () ->  service.transfer("ACC-1","ACC-2",500.0));
+            verify(notificationService).sendTransferFailure(eq("ACC-1"),eq(500.0),anyString());
+            verifyNoMoreInteractions(notificationService);
+        }
+    }
+    @Nested class AccountNotFoundTests{
+
+        @Test
+        void shouldThrowExceptionWhenSenderAccountNotFound(){
+            //arrange
+            when(accountRepository.findByAccountNumber("ACC-FAKE")).thenReturn(Optional.empty());
+            //act + assert
+            assertThrows(AccountNotFoundException.class, () -> service.transfer("ACC-FAKE","ACC-2",1000));
+            verifyNoInteractions(auditLogger,notificationService);
+        }
+        @Test
+        void shouldThrowExceptionWhenReceiverAccountNotFound(){
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.empty());
+            //act + assert
+            assertThrows(AccountNotFoundException.class, () -> service.transfer("ACC-1","ACC-2",100));
+            verifyNoInteractions(auditLogger,notificationService);
+
+        }
+    }
+    @Nested class ValidationTests{
+        @Test
+        void shouldThrowExceptionForNegativeAmount(){
+            //act + assert
+           IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> service.transfer("ACC-1","ACC-2",-100));
+           assertThat(ex.getMessage()).isEqualTo("Kwota przelewu musi być większa od zera");
+            verifyNoInteractions(accountRepository, auditLogger, notificationService);
+        }
+        @Test
+        void shouldThrowExceptionForZeroAmount(){
+
+        }
+    }
+    @Nested class DailyLimitTest{
+        @BeforeEach
+        void setUp(){
+            senderAccount = new Account("ACC-1", "Jan Kowalski", 1000.0);
+            receiverAccount = new Account("ACC-2", "Anna Nowak", 500.0);
+        }
+        @Test
+        void shouldThrowDailyLimitException(){
+            DailyLimitExceededException ex = assertThrows(DailyLimitExceededException.class, () -> service.transferWithLimit("ACC-1","ACC-2",200,190));
+            assertThat(ex.getMessage()).isEqualTo("Kwota ktora chesz wyslac przekracza Twoj dzienny limit");
+            verify(auditLogger).logTransfer(any());
+            verifyNoMoreInteractions(notificationService,auditLogger,accountRepository);
+        }
+        @Test
+        void shouldReceiveWithinTheLimit(){
+            //arrange
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.of(receiverAccount));
+            //act
+            service.transferWithLimit("ACC-1","ACC-2",500,1000);
+            //assert
+            assertThat(receiverAccount.getBalance()).isEqualTo(1000);
+            assertThat(senderAccount.getBalance()).isEqualTo(500);
+            verify(notificationService).sendTransferConfirmation("ACC-1",500,"ACC-2");
+            verify(auditLogger).logTransfer(any());
+            verify(accountRepository).save(senderAccount);
+            verify(accountRepository).save(receiverAccount);
+        }
+        @Test
+        void shouldReceiveWhenAmountEqualsLimit(){
+            when(accountRepository.findByAccountNumber("ACC-1")).thenReturn(Optional.of(senderAccount));
+            when(accountRepository.findByAccountNumber("ACC-2")).thenReturn(Optional.of(receiverAccount));
+            //act
+            service.transferWithLimit("ACC-1","ACC-2",500,500);
+            //assert
+            assertThat(receiverAccount.getBalance()).isEqualTo(1000);
+            assertThat(senderAccount.getBalance()).isEqualTo(500);
+            verify(notificationService).sendTransferConfirmation("ACC-1",500,"ACC-2");
+            verify(auditLogger).logTransfer(any());
+            verify(accountRepository).save(senderAccount);
+            verify(accountRepository).save(receiverAccount);
+        }
+    }
+
+
 }
